d11bac7dd3739e63c979eff67ba5d369773d6fde
David <davidserranoblazquez@gmail.com>
2024-04-22 17:21:51 +0200

    Estructura base MVC

---
+++ src/main/java/com/microservice/controller/SensorEventController.java
@@ -0,0 +1,28 @@
+package com.microservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.model.SensorEvent;
+import com.microservice.service.SensorEventService;
+import com.microservice.service.SensorEventServiceImpl;
+
+@RestController
+@RequestMapping(value = "/sensorevent")
+public class SensorEventController {
+
+    @Autowired
+    private SensorEventServiceImpl sensorEventService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createSensorEvent(@RequestBody SensorEvent sensorEvent) {
+
+        sensorEventService.processSensorEvent(sensorEvent);
+        return new ResponseEntity<>("Se ha creado un registro correctamente en la coleccion sensor_events", HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/microservice/service/SensorEventService.java
@@ -0,0 +1,10 @@
+package com.microservice.service;
+
+import com.microservice.model.SensorEvent;
+
+
+public interface SensorEventService {
+
+    public void processSensorEvent(SensorEvent sensorEvent);
+
+}
